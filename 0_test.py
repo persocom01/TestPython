bd91aefdc474d70fe50f7e1681257103be70ed6b
persocom01 <persocom02@gmail.com>
2019-01-17 15:13:36 +0800

    test

--- 0_test.py
+++
@@ -1,6 +0,0 @@
-database =
-
-list = []
-for ref in database:
-    if ref == '':
-        list.append(ref)
--- 3_if_test.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Please enter an integer: '))
-
-if x == 0:
-    print('you entered zero')
-elif x > 0:
-    print('{} is greater than zero'.format(x))
-else:
-    print(str(x) + ' is smaller than zero')
