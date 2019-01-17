787c60131a864b2b6ac31edc712b1c487e3f3fe3
persocom01 <persocom02@gmail.com>
2019-01-17 14:09:58 +0800

    1.0

---
+++ mypackage/childpackage/mod2.py
@@ -0,0 +1,5 @@
+def goodbye(x):
+    print('goodbye, ' + x)
+
+def goodbye2(x):
+    print('see you again, ' + x)
---
+++ mypackage/mod1.py
@@ -0,0 +1,5 @@
+def question(x):
+    print('how about lunch, ' + x + '?')
+
+def reply(x):
+    print('no thank you, ' + x)
