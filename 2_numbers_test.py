dea76cb85bf7799eecb695135a8b0a2ded7b34b4
persocom01 <persocom02@gmail.com>
2019-01-24 03:29:35 +0800

    1.6

--- 2_numbers_test.py
+++ 2_numbers_test.py
@@ -1,1 +1,20 @@
 # Demonstrates common functions used with numbers.
+import math
+
+# Absolute value.
+print(abs(-123), '\n')
+
+# Powers.
+x = 5**2
+print(x)
+print(pow(5, 2))
+print(math.sqrt(25), '\n')
+
+# Larger/smaller number.
+print(max(5, 2))
+print(min(5, 2), '\n')
+
+# Rounding.
+print(round(1.5))
+print(math.floor(1.7))
+print(math.ceil(1.2), '\n')
--- 9_list_test.py
+++ 9_list_test.py
@@ -15,13 +15,19 @@ fruits.append('grape')
 fruits.extend(['mango', 'papaya'])
 print('Append:', fruits)
 
+# Of note is the face remove only removes the first matching element.
+fruits.remove('apple')
+print('Remove:', fruits)
+
 fruits.sort()
 print('Sort:', fruits)
 
 # Removes last entry and returns it. Can be used with append to make a stack.
 print(fruits.pop())
 print('pop:', fruits)
-print()
+
+# Delates list.
+print(fruits.clear(), '\n')
 
 # Imports a type of list where append and pop functions can be applied to the
 # beginning.
