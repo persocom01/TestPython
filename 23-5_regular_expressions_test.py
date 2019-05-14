794a99c275a69f36cd63bda0d395e5344e96d718
persocom01 <persocom02@gmail.com>
2019-05-14 13:46:43 +0800

    2.22

--- 23-5_regular_expressions_test.py
+++ 23-5_regular_expressions_test.py
@@ -53,8 +53,9 @@ print()
 website_names = re.findall(
     r'\bhttps?://\w+\.\w+(?:\.\w+)?(?:/\+?\w+)*/?(?:\.[a-z]{2,4})?', text, re.I | re.M)
 # Demonstrates use of negative lookahead to exclude www website names.
-# (?!www) ensures that https:// is NOT followed by www.
-# (?<!) is lookbehind. (?=) is positive lookahead.
+# (?!www) is called negative lookahead. It ensures that https:// is NOT followed by www.
+# Replacing ! with = makes it positive. (?=) is positive lookahead.
+# (?<!) is lookbehind.
 non_www_website_names = re.findall(
     r'\bhttps?://(?:(?!www)\w+)\.\w+(?:\.\w+)?(?:/\+?\w+)*/?(?:\.[a-z]{2,4})?', text, re.I | re.M)
 print('websites:')
