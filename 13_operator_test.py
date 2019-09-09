dbe5b317cc8d343a2fa4838ff68c36b6f9d7e7ef
persocom01 <persocom02@gmail.com>
2019-09-09 14:16:44 +0800

    2.25

--- 13_operator_test.py
+++ 13_operator_test.py
@@ -6,8 +6,9 @@
 # var.method
 # **
 
-# Demonstrates use of conditions in variables.
+# Demonstrates use of conditions in variables.]
+# The logical operators are and, or and not.
 # and and or are evaluated from left to right.
 letter1, letter2, letter3 = 'a', '', 'c'
-non_null = letter1 and letter2 and letter3
+non_null = letter1 or letter2 and letter3
 print(non_null)
