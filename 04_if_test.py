ff2f5125fba4c07e9efd7e1e7dffd18e9bd3231b
persocom01 <persocom02@gmail.com>
2020-02-18 16:33:18 +0800

    2.56

--- 04_if_test.py
+++ 04_if_test.py
@@ -1,9 +1,17 @@
-# Demonstrates bsic if statement, as well as how to ask for input.
+# Demonstrates the basic if statement, as well as how to ask for input.
 x = int(input('Please enter an integer: '))
+
+# While not specified in pep8, one convention in naming boolean variables is
+# to start variables or methods with is or has.
+
+
+def isxzero(x):
+    return x == 0
+
 
 # Technically you can write if statements in 1 line, such as
 # if x == 0: do something.
-if x == 0:
+if isxzero(x):
     print('you entered zero')
 # It is possible to use operators like this in python, but not
 # in vba.
