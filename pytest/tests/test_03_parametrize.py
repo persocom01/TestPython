7486ea6a44ea51a9af76840488aef192908b1003
persocom01 <persocom02@gmail.com>
2021-06-02 15:56:22 +0800

    2.72

--- pytest/tests/test_03_parametrize.py
+++ pytest/tests/test_03_parametrize.py
@@ -3,13 +3,16 @@ import math
 import pytest
 
 
-@pytest.mark.parametrize("i, o", [(25, 30), (9, 3)])
+# Note how it is possible to combine groupings with parametrize.
+@pytest.mark.math
+@pytest.mark.parametrize('i, o', [(25, 30), (9, 3)])
 def test_param_sqrt(input, i, o):
     n = i
     assert math.sqrt(n) + input == o
 
 
-@pytest.mark.parametrize("i, o", [(25, 49), (7, 49)])
+@pytest.mark.others
+@pytest.mark.parametrize('i, o', [(25, 49), (7, 49)])
 def test_param_square(i, o):
     n = i
     assert pow(n, 2) == o
