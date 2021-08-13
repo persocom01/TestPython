aaf76838398db48c6ead2515955b0203b9219b4a
persocom01 <persocom02@gmail.com>
2021-08-13 16:02:23 +0800

    2.74

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,3 +1,6 @@
+# The __init__.py is used to cause python to identify the folder as an
+# importable package. It is normally kept blank, but can be written such that
+# only specific files and functions can be imported.
 # Demonstrates use of relative import for packages.
 from . import mod1
 # Absolute import still usable.
