c5e40548493de5436c7239a2302256167379b6ea
persocom01 <persocom02@gmail.com>
2021-10-17 16:15:56 +0800

    2.83

--- fastapi/vosk_stt.py
+++ fastapi/vosk_stt.py
@@ -13,21 +13,21 @@ class Vosk:
         if hasattr(self, '_model'):
             return self._model
         else:
-            print('the model attribute does not exist')
+            print('[vosk_stt]The model attribute does not exist')
 
     @model.setter
     def model(self, value):
         if os.path.exists(value):
             self._model = Model(value)
         else:
-            print("Model not found. Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+            print("[vosk_stt]Model not found. Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
 
     def text_from_sound_file(self, file):
         SetLogLevel(0)
 
         wf = wave.open(file, 'rb')
         if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != 'NONE':
-            print('Audio file must be WAV format mono PCM.')
+            print('[vosk_stt]Audio file must be WAV format mono PCM.')
             exit(1)
 
         rec = KaldiRecognizer(self._model, wf.getframerate())
