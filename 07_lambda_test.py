e4709fbb6a5349edde6c88e2c76785f25bd520a7
persocom01 <persocom02@gmail.com>
2022-05-25 06:30:06 +0800

    2.92

--- 07_lambda_test.py
+++ 07_lambda_test.py
@@ -1,18 +1,18 @@
-from functools import reduce
-
 # Demonstration of various ways to use lambda functions.
+from functools import reduce
 
 
 def signal(x): return 'Buy' if x <= 25 else 'Sell' if x >= 75 else 'No change'
 
 
-print(signal(80))
-
 data = [50, 10, 90]
 action = list(map(signal, data))
+print('function result:', action)
+# The lambda function used here is the equivalent of the function above
+action = list(map(lambda x: 'Buy' if x <= 25 else 'Sell' if x >= 75 else 'No change', data))
 # map functions can alternatively be written as list comprehensions like this:
 # action = [signal(x) for x in data]
-print(action)
+print('lambda result:', action)
 
 action = list(filter(lambda x: x == 'Buy' or x == 'Sell', action))
 # filter functions can alternatively be written as list comprehensions:
